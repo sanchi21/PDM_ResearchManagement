e36a12b6bd46f71eb1c4b99cdbee1308f569419d
jeyaprashanth <jprashanth110993@gmail.com>
2015-02-01 14:01:07 +0530

    PDM SQL

---
+++ SQL/User.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 01, 2015 at 08:23 AM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `PDM`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `First_Name` varchar(100) DEFAULT NULL,
+  `Last_Name` varchar(100) NOT NULL,
+  `Staff_ID` varchar(12) NOT NULL,
+  `NIC` varchar(10) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `Moblile` int(10) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `Year` year(4) NOT NULL,
+  `Last_Access` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Staff_ID`),
+  UNIQUE KEY `NIC` (`NIC`,`Moblile`,`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
